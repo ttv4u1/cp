477d298a4f0848640b25b2d69d67e1526f27f3d0
Foyuu <134723809+ttv4u1@users.noreply.github.com>
2026-04-15 15:20:19 +0800

    Update coi-serviceworker.js

--- coi-serviceworker.js
+++ coi-serviceworker.js
@@ -1,1 +1,25 @@
-<script src="coi-serviceworker.js"></script>
+/*! coi-serviceworker v0.1.7 - MIT License - https://github.com/gzuidhof/coi-serviceworker */
+if (typeof window === "undefined") {
+    self.addEventListener("install", () => self.skipWaiting());
+    self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+    self.addEventListener("fetch", (event) => {
+        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") return;
+        event.respondWith(
+            fetch(event.request).then((response) => {
+                if (response.status === 0) return response;
+                const newHeaders = new Headers(response.headers);
+                newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+                newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+                return new Response(response.body, { status: response.status, statusText: response.statusText, headers: newHeaders });
+            }).catch((e) => console.error(e))
+        );
+    });
+} else {
+    const script = document.currentScript;
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register(window.location.pathname + "coi-serviceworker.js").then((registration) => {
+            registration.addEventListener("updatefound", () => location.reload());
+            if (registration.active && !navigator.serviceWorker.controller) location.reload();
+        });
+    }
+}
